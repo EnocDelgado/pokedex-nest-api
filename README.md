3f4f01491fd1084b9c759c2094764f0e236a4f7e
Enoc Delgado <105158271+EnocDelgado@users.noreply.github.com>
2023-07-04 10:49:05 -0500

    Section 8 finished

--- README.md
+++ README.md
@@ -23,6 +23,13 @@ npm i -g @nestjs/cli
 docker-compose up -d
 ```
 
+5. Rebuilt database with seed, make a get request petition with:
+```
+http://localhost:3000/api/v2/seed
+```
+
+
+
 ## Used Stack
 * MongoDB
 * Nest
--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,25 +1,44 @@
+
 import { Injectable } from '@nestjs/common';
-import { CreateSeedDto } from './dto/create-seed.dto';
-import { UpdateSeedDto } from './dto/update-seed.dto';
-import axios, { AxiosInstance } from 'axios';
-import { PokeResponse } from './interfaces/poke_response';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+
+import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
+import { PokeResponse } from './interfaces/poke_response.interface';
+import { AxiosAdapter } from 'src/common/adapter/axios.adapter';
 
 @Injectable()
 export class SeedService {
 
-  private readonly axios: AxiosInstance = axios;
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly http: AxiosAdapter,
+  ){}
 
   async executeSeed() {
 
-    const { data } = await this.axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=10');
+    // delete * from pokemon if exist any data
+    await this.pokemonModel.deleteMany({}) 
 
-    data.results.forEach( ({ name, url }) => {
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+
+    const pokemonToInsert: { name: string, no: number }[] = [];
+
+    data.results.forEach( async ({ name, url }) => {
 
       const segments = url.split('/');
       const no = +segments[ segments.length - 2];
-    });
 
-    return data.results;
+      // const pokemon = await this.pokemonModel.create({ name, no });
+      pokemonToInsert.push({ name, no });
+
+    });
+    // add all pokemon on our array
+    await this.pokemonModel.insertMany( pokemonToInsert );
+
+    return 'Seed Executed';
   }
 
 }
